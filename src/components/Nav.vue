e9bbc9fa76e54bc1406098bad0d6455908070e06
RanuDewantara <ranuputradewantara@programmer.net>
2025-03-03 22:52:21 +0700

    update: fix navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -7,7 +7,7 @@
 					<span class="h-52 bg-transparent border-4 text-sky-700 ps-1 pe-2 rounded-e-full">Photo</span>
 				</div>
 			</a>
-			<button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 me-3 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-[#1C0000] focus:outline-none" aria-controls="navbar-solid-bg" aria-expanded="false">
+			<button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 me-3 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg bg-[#101010] hover:bg-black focus:outline-none md:hidden" aria-controls="navbar-solid-bg" aria-expanded="false">
 				<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
 					<path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
 				</svg>
